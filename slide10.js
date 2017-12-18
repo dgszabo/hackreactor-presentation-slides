3f9509ba83e97515a789c8ffeb94b9b6af512160
Daniel Szabo <dgszabo@gmail.com>
2017-12-18 15:36:07 -0800

    slide 10 function added

--- slide10.js
+++ slide10.js
@@ -4,7 +4,13 @@
 
 
 
-                      Write a function that adds
+/*                      Write a function that adds
                       from two invocations.
 
                         addf(3)(4) //=> 7
+*/
+function addf(num1) {
+	return function(num2) {
+		return num1 + num2;
+	}
+}
